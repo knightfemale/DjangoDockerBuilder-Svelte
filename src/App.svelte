8c501583f00b36986eef37345bbedecbad8f2ffe
骑士姬 <2067834160@qq.com>
2025-11-07 21:13:31 +0800

    feat: 配置跨域

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,24 @@
-<script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+<script>
+    import { onMount } from "svelte";
+    
+    let backendData = "正在请求后端...";
+    
+    onMount(() => {
+        fetch("/api/core/hello")
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`网络响应不正常 (${response.status})`);
+                }
+                return response.text();
+            })
+            .then((text) => {
+                backendData = text;
+            })
+            .catch((error) => {
+                backendData = `无法从后端加载数据: ${error.message}`;
+            });
+    });
 </script>
 
-<main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+<p class="text-3xl font-bold">后端请求测试:</p>
+<p class="text-3xl underline">{backendData}</p>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-})
+  server: {
+    host: "0.0.0.0",
+    port: 80,
+    strictPort: true,
+    proxy: {
+      "/api": {
+        target: "http://localhost:30001",
+        changeOrigin: true,
+        secure: false,
+      },
+      "/media": {
+        target: "http://localhost:30000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  optimizeDeps: {
+    exclude: ["svelte"],
+  },
+  build: {
+    outDir: "../docker/frontend/html/dist",
+    emptyOutDir: true,
+  },
+});
